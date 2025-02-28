478e0d4b686a6d9dbe5625e6d9b244d2fa3c7022
imanefjer <imane.fjer@gmail.com>
2025-02-28 17:04:18 +0000

    add a readme and linkedlist extension

--- main.go
+++ main.go
@@ -1,64 +1,98 @@
 package main
 
 import (
-	"cool-compiler/lexer"
-	"cool-compiler/parser"
-	"cool-compiler/semant"
 	"flag"
 	"fmt"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"strings"
+
+	"cool-compiler/codegen"
+	"cool-compiler/lexer"
+	"cool-compiler/parser"
+	"cool-compiler/semant"
 )
 
-func main3() {
+func main() {
+	// Get the COOL input file via the -i flag.
 	inputFilePath := flag.String("i", "", "Path to the COOL source file")
 	flag.Parse()
-
 	if *inputFilePath == "" {
-		fmt.Println("Error: Input file path is required.")
+		fmt.Println("Error: Input file path is required. Use -i <filename.cool>")
 		os.Exit(1)
 	}
 
-	code, err := os.ReadFile(*inputFilePath)
+	// Read the COOL source code from the file.
+	codeBytes, err := os.ReadFile(*inputFilePath)
 	if err != nil {
-		fmt.Printf("Error reading input file: %v %v\n", err, *inputFilePath)
+		fmt.Printf("Error reading file %s: %v\n", *inputFilePath, err)
 		os.Exit(1)
 	}
 
-	// Debug: Print the input
-	fmt.Println("\n=== Input Program ===")
-	fmt.Println(string(code))
+	// Preprocess imports
+	baseDir := filepath.Dir(*inputFilePath)
+	code, err := parser.PreprocessImports(string(codeBytes), baseDir)
+	if err != nil {
+		fmt.Printf("Error processing imports: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("=== Input Program ===")
+	fmt.Println(code)
 	fmt.Println("\n=== Starting Compilation ===")
 
-	// Create lexer and parser
-	l := lexer.NewLexer(strings.NewReader(string(code)))
-	p := parser.New(l)
+	// Create lexer and parser with preprocessed code
+	lex := lexer.NewLexer(strings.NewReader(code))
+	parser := parser.New(lex)
 
-	// Parse the program
-	program := p.ParseProgram()
-	if len(p.Errors()) != 0 {
-		fmt.Println("\n=== Parser Errors ===")
-		for _, err := range p.Errors() {
+	// Parse the program.
+	program := parser.ParseProgram()
+	if len(parser.Errors()) != 0 {
+		fmt.Println("=== Parser Errors ===")
+		for _, err := range parser.Errors() {
 			fmt.Println(err)
 		}
 		os.Exit(1)
 	}
 
-	fmt.Println("\n=== Parsing Successful ===")
-	fmt.Println("\n=== Starting Semantic Analysis ===")
-
-	// Create and run semantic analyzer
+	fmt.Println(parser.PrintAST(program))
+	// Run semantic analysis.
 	analyzer := semant.NewSemanticAnalyzer()
 	analyzer.Analyze(program)
-
-	// Check for semantic errors
 	if len(analyzer.Errors()) != 0 {
-		fmt.Println("\n=== Semantic Errors ===")
+		fmt.Println("=== Semantic Errors ===")
 		for _, err := range analyzer.Errors() {
 			fmt.Println(err)
 		}
 		os.Exit(1)
 	}
+	fmt.Println("=== Semantic Analysis Successful ===")
 
-	fmt.Println("\n=== Semantic Analysis Successful ===")
+	// Generate LLVM IR.
+	generator := codegen.NewCodeGenerator()
+	module, err := generator.Generate(program)
+	if err != nil {
+		fmt.Printf("Error generating LLVM IR: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("\n=== Generated LLVM IR ===")
+
+	// Write the LLVM IR to a file.
+	irFilename := strings.TrimSuffix(*inputFilePath, ".cool") + ".ll"
+	if err := os.WriteFile(irFilename, []byte(module.String()), 0644); err != nil {
+		fmt.Printf("Error writing IR to file %s: %v\n", irFilename, err)
+		os.Exit(1)
+	}
+
+	// Run the generated IR using lli.
+	fmt.Printf("\n=== Running LLVM IR with lli (%s) ===\n", irFilename)
+	cmd := exec.Command("lli", irFilename)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Printf("Error running lli: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Program output:\n%s\n", output)
 }
